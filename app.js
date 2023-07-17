ac023e6eb8db9c63518ccbc5af77a70b07cdc3fd
s-achawro <IDoDis@outlook.com>
2023-07-16 19:46:07 -0700

    moving sql code to the top

--- app.js
+++ app.js
@@ -2,57 +2,6 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
-const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-}
-
-menu.addEventListener('click', mobileMenu)
-
-// Show active menu when scrolling
-const highlightMenu = () => {
-    const elem = document.querySelector('.highlight')
-    const homeMenu = document.querySelector('#home-page')
-    const aboutMenu = document.querySelector('#about-page')
-    const featuresMenu = document.querySelector('#features-page')
-    let scrollPos = window.scrollY
-
-    // adds 'highlight' class to menu items
-    if(window.innerWidth > 960 && scrollPos < 600) {
-        homeMenu.classList.add('highlight')
-        aboutMenu.classList.remove('highlight')
-        return
-    } else if (window.innerWidth > 960 && scrollPos < 1400) {
-        aboutMenu.classList.add('highlight')
-        homeMenu.classList.remove('highlight')
-        featuresMenu.classList.remove('highlight')
-        return
-    } else if (window.innerWidth > 960 && scrollPos < 2345) {
-        featuresMenu.classList.add('highlight')
-        aboutMenu.classList.remove('highlight')
-        return
-    } 
-
-    if(elem && window.innerWidth < 960 && scrollPos < 600 || elem) {
-        elem.classList.remove('highlight')
-    }
-}
-
-window.addEventListener('scroll', highlightMenu)
-window.addEventListener('click', highlightMenu)
-
-// Close mobile Menu when clicking again //
-const hideMobileMenu = () => {
-    const menuBars = document.querySelector('.is-active')
-    if (window.innerWidth <= 768 && menuBars) {
-        menu.classList.toggle('is-active')
-        menuLinks.classList.remove('active')
-    } 
-}
-
-
-
-
 
 const mysql = require('mysql');
 
@@ -106,6 +55,59 @@ connection.query(insertQuery, [users], function (error, results, fields) {
 });
 
 connection.end();
+
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const featuresMenu = document.querySelector('#features-page')
+    let scrollPos = window.scrollY
+
+    // adds 'highlight' class to menu items
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        featuresMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        featuresMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+    } 
+
+    if(elem && window.innerWidth < 960 && scrollPos < 600 || elem) {
+        elem.classList.remove('highlight')
+    }
+}
+
+window.addEventListener('scroll', highlightMenu)
+window.addEventListener('click', highlightMenu)
+
+// Close mobile Menu when clicking again //
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    } 
+}
+
+
+
+
 
 
 
