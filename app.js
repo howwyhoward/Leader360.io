9b5a58391b7d2d95bb0e2d09f905c02dcab59664
s-achawro <IDoDis@outlook.com>
2023-07-16 18:54:50 -0700

    added sql code, not called submit button yet

--- app.js
+++ app.js
@@ -50,5 +50,66 @@ const hideMobileMenu = () => {
     } 
 }
 
+
+
+
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'leadershipmysql.mysql.database.azure.com',
+  user     : 'leadershipmysqladmin',
+  password : 'R3dD#ad24398L',
+  database : 'leadershipmysql ',
+  ssl  : {
+    ca : fs.readFileSync('DigiCertGlobalRootG2.crt.pem')
+  }
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting: ' + error.stack);
+    return;
+  }
+  
+  console.log('Connected as id ' + connection.threadId);
+});
+
+let createTableQuery = `
+  CREATE TABLE tblUSER (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    UserFname VARCHAR(30) NOT NULL,
+    UserLname VARCHAR(30) NOT NULL,
+    UserEmail VARCHAR(50) NOT NULL,
+    UserPhoneNumber VARCHAR(20) NOT NULL
+  )
+`;
+
+connection.query(createTableQuery, function (error, results, fields) {
+  if (error) throw error;
+  console.log('Table "tblUSER" has been created');
+});
+
+let users = [
+  ['Arnav', 'Khare', 'arney.chillarney@gmail.com', '4252367685'],
+  ['Ali', 'Chawro', 'achawro9@gmail.com', '4254481587'],
+  ['Howard', 'Wang', 'howardw1120@gmail.com', '4253517712'],
+  ['Santosh', 'Khare', 'santosh@golsinc.com', '4254926980'],
+  ['Atul', 'Khare', 'akhare@gmail.com', '4254436066'],
+];
+
+let insertQuery = 'INSERT INTO tblUSER (UserFname, UserLname, UserEmail, UserPhoneNumber) VALUES ?';
+
+connection.query(insertQuery, [users], function (error, results, fields) {
+  if (error) throw error;
+  console.log('Inserted ' + results.affectedRows + ' rows');
+});
+
+connection.end();
+
+
+
+
+
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
