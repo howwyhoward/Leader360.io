8df542a3c064794b587fd34712e36226ffd5e506
s-achawro <IDoDis@outlook.com>
2023-07-17 20:08:39 -0700

    test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   console.log('in function');
+  console.info('test1');
   $('#user-form').on('submit', function(e) {
       e.preventDefault();
 
@@ -13,6 +14,7 @@ $(document).ready(function() {
           },
           success: function(data) {
               console.log('success');
+              console.info('test');
               $('#result').html(`Full name: ${data.UserFname} ${data.UserLname}`);
           },
           error: function(error) {
