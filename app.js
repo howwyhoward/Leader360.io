fdf69dba0e27cf313953b4de6e5bb6503b69e144
s-achawro <IDoDis@outlook.com>
2023-07-19 17:49:32 -0700

    adding path for public

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var cors = require('cors');
 
 var app = express();
 
+
 app.use(cors());
 app.use(express.json());
+var path = require('path');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -88,3 +90,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
